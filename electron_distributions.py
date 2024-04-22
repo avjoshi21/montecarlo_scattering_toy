c0b82f3027677b9579c3c1df783cafbcccb10bd5
Abhishek Joshi <avjoshi2@illinois.edu>
2024-04-22 00:36:42 -0500

    reverted numpy like implementation as numpy version requires rewrite of hotcross.py as gammae arrays are not the same size. tabled for later.

--- electron_distributions.py
+++ electron_distributions.py
@@ -119,17 +119,17 @@ class ThermalDistribution(DistFunc):
         from hotcross import sigma_kn
         # print(photon_energy,kwargs['thetae'])
         # print(utils.bounds['thetae_min'],utils.bounds['photon_energy_min'])
-        # if(kwargs['thetae'] < utils.bounds['thetae_min'] and photon_energy < utils.bounds['photon_energy_min']):
-        #     return utils.constants['sigma_thomson']
-        # elif (kwargs['thetae'] < utils.bounds['thetae_min']):
-        #     return sigma_kn(photon_energy)
-        # else:
-        #     return -1
+        if(kwargs['thetae'] < utils.bounds['thetae_min'] and photon_energy < utils.bounds['photon_energy_min']):
+            return utils.constants['sigma_thomson']
+        elif (kwargs['thetae'] < utils.bounds['thetae_min']):
+            return sigma_kn(photon_energy)
+        else:
+            return -1
 
-        # numpy way
-        condition_numpy = np.where(kwargs['thetae'] < utils.bounds['thetae_min'] and photon_energy < utils.bounds['photon_energy_min'],utils.constants['sigma_thomson'],\
-            np.where(kwargs['thetae'] < utils.bounds['thetae_min'] and photon_energy >= utils.bounds['photon_energy_min'],sigma_kn(photon_energy),-1))
-        return condition_numpy
+        # # numpy way
+        # condition_numpy = np.where(kwargs['thetae'] < utils.bounds['thetae_min'] and photon_energy < utils.bounds['photon_energy_min'],utils.constants['sigma_thomson'],\
+        #     np.where(kwargs['thetae'] < utils.bounds['thetae_min'] and photon_energy >= utils.bounds['photon_energy_min'],sigma_kn(photon_energy),-1))
+        # return condition_numpy
 
     def test_sampling(self,thetae=10,nsamples=1e3,gamma_min=1,gamma_max=1e3,**kwargs):
         """ tests the sampling procedure by sampling nsamples of gamma from the distribution function and plots out vs. the expected distribution"""
