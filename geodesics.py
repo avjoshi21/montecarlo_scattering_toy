6a00b1bd39c3d2f12e2ac5eec284a542c1f31625
Abhishek Joshi <avjoshi2@illinois.edu>
2024-05-04 18:04:17 -0500

    better organization of geometry (techincally coordinate) specific functions

--- geodesics.py
+++ geodesics.py
@@ -1,80 +1,10 @@
 import numpy as np
-
+import geometry
 
 class FlatspaceSphericalGeodesics:
     def __init__(self):
+        self.flat_geom = geometry.MinkowskiSpherical()
         pass
-
-    def cart_to_spher(self,X: np.array) -> np.array:
-        """
-        Should definitely not be a function here but in geometry.py
-        """
-        x = X[1]
-        y = X[2]
-        z = X[3]
-        r=np.sqrt(x**2+y**2+z**2)
-        return np.array([
-            X[0],
-            r,
-            np.arccos(z/r),
-            np.arctan(y/x)
-        ])
-    
-    def spher_to_cart(self,X: np.array) -> np.array:
-        """
-        Should also be in geometry.py, not here
-        """
-        r = X[1]
-        sth = np.sin(X[2])
-        cth = np.cos(X[2])
-        sphi = np.sin(X[3])
-        cphi = np.cos(X[3])
-
-        return np.array([
-            X[0],
-            r * sth * cphi,
-            r * sth * sphi,
-            r * cth
-        ])
-    
-    def dx_cartesian_dx_spherical(self,X: np.array) -> list:
-        """
-        dx^mu(cartesian)/dx^mu(spherical) for flatspace
-        Parameters:
-        X -- x^mu in spherical
-        Returns:
-        dx^mu(cartesian)/dx^mu(spherical) for flatspace
-        """
-        r = X[1]
-        sth = np.sin(X[2])
-        cth = np.cos(X[2])
-        sphi = np.sin(X[3])
-        cphi = np.cos(X[3])
-        return np.array([
-            [1,0,0,0],
-            [0,sth*cphi,X[1]*cth*cphi,-r*sth*sphi],
-            [0,sth*sphi,r*cth*sphi,r*sth*cphi],
-            [0,cth,-r*sth,0]
-            ])
-
-    def dx_spherical_dx_cartesian(self,Xcart: np.array) -> list:
-        """
-        dx^mu(spherical)/dx^mu(cartesian) for flatspace
-        Parameters:
-        Xcart -- x^mu in cartesian
-        Returns:
-        dx^mu(spherical)/dx^mu(cartesian) for flatspace
-        """
-        x = Xcart[1]
-        y = Xcart[2]
-        z = Xcart[3]
-        r = np.sqrt(x**2+y**2+z**2)
-        return np.array([
-            [1,0,0,0],
-            [0,x/r,y/r,z/r],
-            [0, x*z/(np.sqrt(x**2+y**2)*r**2), y*z/(np.sqrt(x**2+y**2)*r**2), -np.sqrt(x**2+y**2)/r**2],
-            [0, -y/(x**2+y**2), x/(x**2+y**2), 0]
-        ])
 
     
 
@@ -91,15 +21,15 @@ class FlatspaceSphericalGeodesics:
         finalX: final x^mu of superphoton in spherical
         finalK: final k^mu of superphoton in spherical
         """
-        jacobi_spher_to_cart  = self.dx_cartesian_dx_spherical(initX)
+        jacobi_spher_to_cart  = self.flat_geom.dx_cartesian_dx_spherical(initX)
         # print(jacobi_spher_to_cart)
-        initX_cart = self.spher_to_cart(initX)
+        initX_cart = self.flat_geom.spher_to_cart(initX)
         initK_cart = np.einsum("ij,j->i",jacobi_spher_to_cart,initK)
 
         finalX_cart = initX_cart + initK_cart*dl
         finalK_cart = initK_cart
-        jacobi_cart_to_spher = self.dx_spherical_dx_cartesian(finalX_cart)
-        finalX = self.cart_to_spher(finalX_cart)
+        jacobi_cart_to_spher = self.flat_geom.dx_spherical_dx_cartesian(finalX_cart)
+        finalX = self.flat_geom.cart_to_spher(finalX_cart)
         finalK = np.einsum("ij,j->i",jacobi_cart_to_spher,finalK_cart)
 
         if debug:
@@ -115,6 +45,7 @@ def test_flat_geodesic():
     init_K = [1,1,1,0]
     flatspace_geodesic = FlatspaceSphericalGeodesics()
     final_X,final_K = flatspace_geodesic.evolve_geodesic(init_X,init_K,dl);
+    print(init_X,init_K)
     print(final_X,final_K)
 
 if __name__ == "__main__":
--- geometry.py
+++ geometry.py
@@ -19,4 +19,83 @@ class MinkowskiSpherical(Geometry):
         gcov[2][2] = x[1]**2
         gcov[3][3] = x[1]**2 * np.sin(x[2])**2
         return gcov
-        
+
+    def cart_to_spher(self,X: np.array) -> np.array:
+        """
+        Should this be in a coordinates.py?
+        Parameters:
+        X -- position four-vector in cartesian
+        Returns:
+        Position four-vector in spherical
+        """
+        x = X[1]
+        y = X[2]
+        z = X[3]
+        r=np.sqrt(x**2+y**2+z**2)
+        return np.array([
+            X[0],
+            r,
+            np.arccos(z/r),
+            np.arctan(y/x)
+        ])
+    
+    def spher_to_cart(self,X: np.array) -> np.array:
+        """
+        Should be in coordinates.py?
+        Parameters:
+        X -- position four-vector in spherical
+        Returns:
+        Position four-vector in cartesian
+        """
+        r = X[1]
+        sth = np.sin(X[2])
+        cth = np.cos(X[2])
+        sphi = np.sin(X[3])
+        cphi = np.cos(X[3])
+
+        return np.array([
+            X[0],
+            r * sth * cphi,
+            r * sth * sphi,
+            r * cth
+        ])
+    
+    def dx_cartesian_dx_spherical(self,X: np.array) -> list:
+        """
+        dx^mu(cartesian)/dx^mu(spherical) for flatspace
+        Parameters:
+        X -- x^mu in spherical
+        Returns:
+        dx^mu(cartesian)/dx^mu(spherical) for flatspace
+        """
+        r = X[1]
+        sth = np.sin(X[2])
+        cth = np.cos(X[2])
+        sphi = np.sin(X[3])
+        cphi = np.cos(X[3])
+        return np.array([
+            [1,0,0,0],
+            [0,sth*cphi,X[1]*cth*cphi,-r*sth*sphi],
+            [0,sth*sphi,r*cth*sphi,r*sth*cphi],
+            [0,cth,-r*sth,0]
+            ])
+
+    def dx_spherical_dx_cartesian(self,Xcart: np.array) -> list:
+        """
+        dx^mu(spherical)/dx^mu(cartesian) for flatspace
+        Parameters:
+        Xcart -- x^mu in cartesian
+        Returns:
+        dx^mu(spherical)/dx^mu(cartesian) for flatspace
+        """
+        x = Xcart[1]
+        y = Xcart[2]
+        z = Xcart[3]
+        r = np.sqrt(x**2+y**2+z**2)
+        return np.array([
+            [1,0,0,0],
+            [0,x/r,y/r,z/r],
+            [0, x*z/(np.sqrt(x**2+y**2)*r**2), y*z/(np.sqrt(x**2+y**2)*r**2), -np.sqrt(x**2+y**2)/r**2],
+            [0, -y/(x**2+y**2), x/(x**2+y**2), 0]
+        ])
+   
